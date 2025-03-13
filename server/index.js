eed2c67949e58a4662f42c1f48fa8cd80557a3b0
weslife911 <mfonfubikoma@gmail.com>
2025-03-13 18:27:52 +0000

    testing

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const connectToDB = require("./database/db");
 const { registerUser, loginUser, getAllUsers, getUser } = require("./controllers/AuthController");
 const { addCategory, getAllCategories, getCategory, updateCategory } = require("./controllers/CategoryController");
 const { addProducts, getProducts, getProduct } = require("./controllers/ProductController");
-const { Category } = require("./models/Category");
 
 const app = express();
 
@@ -20,30 +19,21 @@ app.get("/", (req, res) => {
 
 // Users
 
-app.post("/api/auth/register", registerUser);
-app.post("/api/auth/login", loginUser);
+app.post("/auth/register", registerUser);
+app.post("/auth/login", loginUser);
 app.get("/api/users", getAllUsers);
 app.get("/api/user/:id", getUser);
 
 // Categories
 
 app.post("/api/add_category", addCategory);
-app.get("/api/categories", async(req, res) => {
-    const allCategories = await Category.find({});
-
-    if(!allCategories.length > 0) return res.json({
-        success: false,
-        message: "Categories could not be found"
-    });
-
-    res.status(200).json(allCategories);
-});
+app.get("/api/categories", getAllCategories);
 app.get("/api/categories/:id", getCategory);
 app.put("/api/category/update/:id", updateCategory);
 
 // Products
 
-app.post("/api/add_prooduct", addProducts);
+app.post("/api/add_product", addProducts);
 app.get("/api/products", getProducts);
 app.get("/api/product/:id", getProduct);
 
--- server/routes/user_routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { registerUser, loginUser } = require("../controllers/AuthController");
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-module.exports = router;
